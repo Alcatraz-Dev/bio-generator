c23a098c399c1ba3a4d0f300a8157a2203513fc3
Alcatraz Deve <alcatraz3019@gmail.com>
2023-02-03 03:51:39 +0100

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center w-full mt-5  pb-7 sm:px-4 px-2">
+      <Link href="/" className="flex space-x-3 ">
+        <h1 className="sm:text-4xl text-2xl font-extrabold ml-2 tracking-tight">
+          Bio Generator AI
+        </h1>
+      </Link>
+    </header>
+  );
+}
